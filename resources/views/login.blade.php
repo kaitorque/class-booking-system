7facc0508e39e2b05293ff5a76c59548da6b6f14
Kaitorque <kaitorque@gmail.com>
2020-07-22 00:59:11 +0800

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,7 +17,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <head><base href="../../../">
         <meta charset="utf-8"/>
         <title>Quantum Eduhub</title>
-        <meta name="description" content="Login page example"/>
+        <meta name="description" content="Quantum Eduhub Login"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
         <!--begin::Fonts-->
