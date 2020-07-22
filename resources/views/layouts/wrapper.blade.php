04f03e9211221d8e7dbbd91a119120d25bb29dd6
Kaitorque <kaitorque@gmail.com>
2020-07-22 10:51:55 +0800

    Update wrapper.blade.php

--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -133,14 +133,14 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--begin::Copyright-->
 		<div class="text-dark order-2 order-md-1">
 			<span class="text-muted font-weight-bold mr-2">2020&copy;</span>
-			<a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Quantum EDU</a>
+			<a href="" target="_blank" class="text-dark-75 text-hover-primary">Quantum EDU</a>
 		</div>
 		<!--end::Copyright-->
 
 		<!--begin::Nav-->
 		<div class="nav nav-dark order-1 order-md-2">
-			<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pr-3 pl-0">About</a>
-			<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link px-3">FAQ</a>
+			<a href="" target="_blank" class="nav-link pr-3 pl-0">About</a>
+			<a href="" target="_blank" class="nav-link px-3">FAQ</a>
 		</div>
 		<!--end::Nav-->
 	</div>
